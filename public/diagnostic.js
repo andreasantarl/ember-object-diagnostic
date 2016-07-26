500ed8d57eb4c5371857e39b6256bef14ffbff6d
andreasantarl <andrea.santarl@gmail.com>
2016-07-26 09:31:53 -0400

    Responses to Ember Object Diagnostic
    Responses

--- public/diagnostic.js
+++ public/diagnostic.js
@@ -8,6 +8,14 @@
 //  -  a quantity
 //  -  a computed property called `orderPrice`, equal to price times quantity
 //
+
+const Order = Ember.Object.extend({
+  orderPrice: Ember.computed('price', 'quantity', function(){
+    let price = price;
+    let quantity = quantity;
+    return price * quantity;
+  })
+});
 // A Cart should have
 //  -  an `addToCart` method, which adds a given Item to an array
 //      called `orders` (HINT: You'll probably need to look through the
@@ -17,9 +25,25 @@
 //      recalculated any time an Order is added to the cart, removed from the
 //      cart, or modified.
 //
+const Cart = Order.extend({
+  orders: [],
+  addToCart: Ember.computed('order', function(){
+    let orders = this.get('orders');
+    orders.pushObject('order');
+  }),
+  totalPrice: Ember.computed.sum('orderPrice'),
+});
+
 // Once you've created the necessary Ember Classes, create a new Cart instance,
 //  and fill that cart up with three new product orders having the following
 //  quantities, product names, and prices:
 //  -  Order 1 : 2 hats ($5 each)
 //  -  Order 2 : 1 desk lamp ($20 each)
 //  -  Order 3 : 3 hand towels ($8 each)
+let newCart = Cart.create ({
+  orders: [
+    Ember.Object.create({ order: 'Hat', quantity: 2, price: 5 }),
+    Ember.Object.create({ order: 'Desk Lamp', quantity: 1, price: 20 }),
+    Ember.Object.create({ order: 'Hand Towl', quantity: 3, price: 8 })
+  ]
+});
